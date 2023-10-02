f3d863480c3211472cce2d2b5f6b2409fd1799a8
unknown <manay@innoclique.com>
2023-10-02 09:50:06 +0530

    manay

---
+++ src/aws-exports.js
@@ -0,0 +1,29 @@
+const awsExports = {
+    "aws_project_region": "us-east-1",
+    "aws_cognito_identity_pool_id": "us-east-1:28f86100-528d-45f1-87d9-df6ec1bb7854",
+    "aws_cognito_region": "us-east-1",
+    "aws_user_pools_id": "us-east-1_oedVL0TiG",
+    "aws_user_pools_web_client_id": "1erjtjj09o3ikg5lkg71iurhot",
+    "oauth": {},
+    "aws_cognito_username_attributes": [
+        "EMAIL"
+    ],
+    "aws_cognito_social_providers": [],
+    "aws_cognito_signup_attributes": [
+        "EMAIL"
+    ],
+    "aws_cognito_mfa_configuration": "OFF",
+    "aws_cognito_mfa_types": [
+        "SMS"
+    ],
+    "aws_cognito_password_protection_settings": {
+        "passwordPolicyMinLength": 8,
+        "passwordPolicyCharacters": []
+    },
+    "aws_cognito_verification_mechanisms": [
+        "EMAIL"
+    ]
+};
+
+
+export default awsExports;
