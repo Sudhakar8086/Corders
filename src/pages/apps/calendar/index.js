902d0784b094550d3305b717eaec856744d73f29
Saikumar-8074 <128357731+Saikumar-8074@users.noreply.github.com>
2023-10-05 11:29:05 +0530

    latest one
    latest

--- src/pages/apps/calendar/index.js
+++ src/pages/apps/calendar/index.js
@@ -1,124 +1,284 @@
-// ** React Imports
-import { useEffect, useState } from 'react'
 
-// ** MUI Imports
-import Box from '@mui/material/Box'
-import useMediaQuery from '@mui/material/useMediaQuery'
+import React, { useState } from 'react';
+import Paper from '@mui/material/Paper';
+import Table from '@mui/material/Table';
+import { styled } from '@mui/material/styles';
+import TableHead from '@mui/material/TableHead';
+import TableBody from '@mui/material/TableBody';
+import TableContainer from '@mui/material/TableContainer';
+import TableRow from '@mui/material/TableRow';
+import TableCell, { tableCellClasses } from '@mui/material/TableCell';
+import Icon from 'src/@core/components/icon'
+const icons=[
+  'mdi:file-outline',
+  'heroicons:folder-20-solid',
+  'mdi:folder-open',
+  'uil:edit-alt',
+  'ci:house-01',
+  'ci:chevron-left-duo',
+  'mdi:application-edit-outline',
+  'bx:file',
+  'openmoji:delete'
+]
 
-// ** Redux Imports
-import { useDispatch, useSelector } from 'react-redux'
+const StyledTableCell = styled(TableCell)(({ theme }) => ({
+  [`&.${tableCellClasses.head}`]: {
+    color: theme.palette.common.white,
+    backgroundColor: theme.palette.common.black,
+    minWidth: '590px', 
+    flex: '20 30 auto',
+  },
+  [`&.${tableCellClasses.body}`]: {
+    fontSize: 14
+  }
+}));
 
-// ** Hooks
-import { useSettings } from 'src/@core/hooks/useSettings'
+const StyledTableRow = styled(TableRow)(({ theme }) => ({
+  '&:nth-of-type(odd)': {
+    backgroundColor: theme.palette.action.hover
+  },
 
-// ** FullCalendar & App Components Imports
-import Calendar from 'src/views/apps/calendar/Calendar'
-import SidebarLeft from 'src/views/apps/calendar/SidebarLeft'
-import CalendarWrapper from 'src/@core/styles/libs/fullcalendar'
-import AddEventSidebar from 'src/views/apps/calendar/AddEventSidebar'
+  // hide last border
+  '&:last-of-type td, &:last-of-type th': {
+    border: 0
+  }
+}));
 
-// ** Actions
-import {
-  addEvent,
-  fetchEvents,
-  deleteEvent,
-  updateEvent,
-  handleSelectEvent,
-  handleAllCalendars,
-  handleCalendarsUpdate
-} from 'src/store/apps/calendar'
+const SimpleTable = () => {
+  const [showSpecialRows, setShowSpecialRows] = useState(false);
+  const [showCoderDocumentSystem, setShowCoderDocumentSystem] = useState(false);
+  const [showAdditionalRow, setShowAdditionalRow] = useState(false);
+  const handleBackClick = () => {
+    setShowSpecialRows(false);
+    setShowCoderDocumentSystem(false);
+    setShowAdditionalRow(false);
+  };
+  const handleFishRoomClick = () => {
+    setShowSpecialRows(false);
+    setShowAdditionalRow(!showAdditionalRow);
+    console.log(showSpecialRows);
+    console.log(showAdditionalRow);
+  };
+  const extraRows = [
+    {
+      icon: (
+        <div className="icon-container">
+          <Icon icon="mdi:folder-open" />
+        </div>
+      ),
+      label: (
+        <div onClick={ handleFishRoomClick} style={{ cursor: 'pointer' }}>
+          Fish Room
+          <div style={{ display: 'flex', alignItems: 'center' }}>
+            <div style={{ marginLeft: 'Auto' }}>
+              <Icon icon="openmoji:delete" />
+              <div>Archive</div>
+            </div>
+            <div style={{ marginLeft: '50PX' }}>
+              <Icon icon="uil:edit-alt" />
+              <div>Rename</div>
+            </div>
+          </div>
+        </div>
+      ),
+    },
+  
+    {
+      icon: (
+        <div className="icon-container">
+          <Icon icon="mdi:folder-open" />
+        </div>
+      ),
+      label: (
+        <div>
+          Nuts Room
+          
 
-// ** CalendarColors
-const calendarsColor = {
-  Personal: 'error',
-  Business: 'primary',
-  Family: 'warning',
-  Holiday: 'success',
-  ETC: 'info'
-}
+          <div style={{ display: 'flex', alignItems: 'center' }}>
+          <div style={{ marginLeft: 'Auto' }}>
+          <Icon icon="openmoji:delete" />
+          <div>Archive</div>
+            </div>
+          <div style={{ marginLeft: '50PX' }}>
+          <Icon icon="uil:edit-alt" />
+                    <div>Rename</div>
+            </div>
+          </div>
+        </div>
+      ),
+    }
+  ];
+ 
+  const additionalRows = [
+    {
+      icon: (
+        <div className="icon-container">
+          <Icon icon="mdi:folder-open" />
+        </div>
+      ),
+      label: (
+        <div>
+          wi-001
+          <div style={{ display: 'flex', alignItems: 'center' }}>
+          <div style={{ marginLeft: 'Auto' }}>
+          <Icon icon="openmoji:delete" />
+          <div>Archive</div>
+            </div>
+          <div style={{ marginLeft: '50PX' }}>
+          <Icon icon="uil:edit-alt" />
+                    <div>Rename</div>
+            </div>
+          </div>
+        </div>
+      ),
+    },
+    {
+      icon: (
+        <div className="icon-container">
+          <Icon icon="mdi:folder-open" />
+        </div>
+      ),
+      label: (
+        <div>
+          wi-003
+        
+          <div style={{ display: 'flex', alignItems: 'center' }}>
+          <div style={{ marginLeft: 'Auto' }}>
+          <Icon icon="openmoji:delete" />
+          <div>Archive</div>
+            </div>
+          <div style={{ marginLeft: '50PX' }}>
+          <Icon icon="uil:edit-alt" />
+                    <div>Rename</div>
+            </div>
+          </div>
+        </div>
+      ),
+    }
+  ];
+  const rowsToDisplay = showSpecialRows ? additionalRows : extraRows;
+  
 
-const AppCalendar = () => {
-  // ** States
-  const [calendarApi, setCalendarApi] = useState(null)
-  const [leftSidebarOpen, setLeftSidebarOpen] = useState(false)
-  const [addEventSidebarOpen, setAddEventSidebarOpen] = useState(false)
-
-  // ** Hooks
-  const { settings } = useSettings()
-  const dispatch = useDispatch()
-  const store = useSelector(state => state.calendar)
-
-  // ** Vars
-  const leftSidebarWidth = 300
-  const addEventSidebarWidth = 400
-  const { skin, direction } = settings
-  const mdAbove = useMediaQuery(theme => theme.breakpoints.up('md'))
-  useEffect(() => {
-    dispatch(fetchEvents(store.selectedCalendars))
-  }, [dispatch, store.selectedCalendars])
-  const handleLeftSidebarToggle = () => setLeftSidebarOpen(!leftSidebarOpen)
-  const handleAddEventSidebarToggle = () => setAddEventSidebarOpen(!addEventSidebarOpen)
-
+  const handleCoderDocumentClick = () => {
+    setShowSpecialRows(!showSpecialRows);
+  };
+  
   return (
-    <CalendarWrapper
-      className='app-calendar'
-      sx={{
-        boxShadow: skin === 'bordered' ? 0 : 6,
-        ...(skin === 'bordered' && { border: theme => `1px solid ${theme.palette.divider}` })
-      }}
-    >
-      <SidebarLeft
-        store={store}
-        mdAbove={mdAbove}
-        dispatch={dispatch}
-        calendarApi={calendarApi}
-        calendarsColor={calendarsColor}
-        leftSidebarOpen={leftSidebarOpen}
-        leftSidebarWidth={leftSidebarWidth}
-        handleSelectEvent={handleSelectEvent}
-        handleAllCalendars={handleAllCalendars}
-        handleCalendarsUpdate={handleCalendarsUpdate}
-        handleLeftSidebarToggle={handleLeftSidebarToggle}
-        handleAddEventSidebarToggle={handleAddEventSidebarToggle}
-      />
-      <Box
-        sx={{
-          p: 6,
-          pb: 0,
-          flexGrow: 1,
-          borderRadius: 1,
-          boxShadow: 'none',
-          backgroundColor: 'background.paper',
-          ...(mdAbove ? { borderTopLeftRadius: 0, borderBottomLeftRadius: 0 } : {})
-        }}
-      >
-        <Calendar
-          store={store}
-          dispatch={dispatch}
-          direction={direction}
-          updateEvent={updateEvent}
-          calendarApi={calendarApi}
-          calendarsColor={calendarsColor}
-          setCalendarApi={setCalendarApi}
-          handleSelectEvent={handleSelectEvent}
-          handleLeftSidebarToggle={handleLeftSidebarToggle}
-          handleAddEventSidebarToggle={handleAddEventSidebarToggle}
-        />
-      </Box>
-      <AddEventSidebar
-        store={store}
-        dispatch={dispatch}
-        addEvent={addEvent}
-        updateEvent={updateEvent}
-        deleteEvent={deleteEvent}
-        calendarApi={calendarApi}
-        drawerWidth={addEventSidebarWidth}
-        handleSelectEvent={handleSelectEvent}
-        addEventSidebarOpen={addEventSidebarOpen}
-        handleAddEventSidebarToggle={handleAddEventSidebarToggle}
-      />
-    </CalendarWrapper>
-  )
-}
+    <>
+   
+    <div style={{ display: 'flex', alignItems: 'center', marginBottom: '10px' }}>
+      {showSpecialRows && (
+        <div onClick={handleBackClick} style={{ cursor: 'pointer', marginRight: '10px' }}>
+          <Icon icon="ci:chevron-left-duo" />
+          Back
+        </div>
+      )}
 
-export default AppCalendar
+<div style={{ display: 'flex', alignItems: 'center', marginRight: '10px' }}>
+|<Icon icon="ci:house-01" />
+        Home
+      </div>
+      <div style={{ display: 'flex', alignItems: 'center', marginRight: '10px' }}>
+       |<Icon icon="mdi:application-edit-outline" />
+        <div style={{ marginLeft: '5px' }}>Add Document</div>
+      </div>
+      <div style={{ display: 'flex', alignItems: 'center', marginRight: '10px' }}>
+     |<Icon icon="bx:file" />
+        <div style={{ marginLeft: '5px' }}>Add Directory</div>
+      </div>
+      </div>
+    <TableContainer component={Paper}>
+      <Table sx={{ minWidth: 700 }} aria-label="customized table">
+        <TableHead>
+          <TableRow>
+            <StyledTableCell>Type</StyledTableCell>
+            <StyledTableCell>Document</StyledTableCell>
+          </TableRow>
+        </TableHead>
+        <TableBody>
+          {showSpecialRows ? (
+            <>
+              {extraRows.map((row, index) => (
+                <StyledTableRow key={index}>
+                  <StyledTableCell>{row.icon}</StyledTableCell>
+                  <StyledTableCell>{row.label}</StyledTableCell>
+                </StyledTableRow>
+              ))}
+            </>
+            
+          ) : (
+            
+            <StyledTableRow onClick={handleCoderDocumentClick}>
+              <StyledTableCell>
+                <div className="icon-container">
+                <Icon icon="mdi:folder-open" />
+                </div>
+              </StyledTableCell>
+              <StyledTableCell>
+                Coder document system
+              </StyledTableCell>
+              <StyledTableCell>
+      <div style={{ display: 'flex', alignItems: 'center' }}>
+                  {name}
+                  <div style={{ marginLeft: '-200PX' }}>
+                  <Icon icon="uil:edit-alt" />
+                    <div>Rename</div>
+                  </div>
+                </div>
+      </StyledTableCell>
+            </StyledTableRow>
+            
+     )}
+      {!showSpecialRows && (
+            <>
+         
+           <StyledTableRow >
+      <StyledTableCell>
+        <div className="icon-container">
+        <Icon icon="mdi:folder-open" />
+        </div>
+      </StyledTableCell>
+      <StyledTableCell>
+       External
+      </StyledTableCell>
+      <StyledTableCell>
+      <div style={{ display: 'flex', alignItems: 'center' }}>
+                 
+                  <div style={{ marginLeft: '-200PX' }}>
+                  <Icon icon="uil:edit-alt" />
+                    <div>Rename</div>
+            
+                  </div>
+                </div>
+      </StyledTableCell>
+    </StyledTableRow>
+    <StyledTableRow >
+      <StyledTableCell>
+        <div className="icon-container">
+        <Icon icon="mdi:folder-open" />
+        </div>
+      </StyledTableCell>
+      <StyledTableCell>
+      Internal
+      </StyledTableCell>
+      <StyledTableCell>
+      <div style={{ display: 'flex', alignItems: 'center' }}>
+              
+                  <div style={{ marginLeft: '-200PX' }}>
+                  <Icon icon="uil:edit-alt" />
+                    <div>Rename</div>
+                  </div>
+                </div>
+      </StyledTableCell>
+    </StyledTableRow>
+    </>
+          )}
+        </TableBody>
+      </Table>
+    </TableContainer>
+    </>
+  );
+};
+
+export default SimpleTable;
+
